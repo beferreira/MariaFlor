1789df1045c6031c65f0da0977bb8f805cdb1400
dujosee <ra44575204891@acaonsfatima.org.br>
2024-11-22 01:23:40 -0300

    CONTROLLER EU AINDA TE PEGO

--- apitcc/src/rotas.js
+++ apitcc/src/rotas.js
@@ -1,15 +1,16 @@
-
-import userController from './Controller/userController.js';
-import produtoController from './controller/produtoController.js'
-import unidadeController from './Controller/unidadeController.js'
-import encomendasController from './Controller/encomendasController.js'
+import cliente from './controller/clienteController.js'
+import usuario from './Controller/userController.js';
+import produto from './controller/produtoController.js'
+import unidade from './Controller/unidadeController.js'
+import encomendas from './Controller/encomendasController.js'
 
 export default function adicionarRotas(servidor){
     
-   servidor.use(userController);
-   servidor.use(produtoController);
-   servidor.use(unidadeController);
-   servidor.use(encomendasController);
+   servidor.use(cliente);
+   servidor.use(usuario);
+   servidor.use(produto);
+   servidor.use(unidade);
+   servidor.use(encomendas);
  
  
 }
