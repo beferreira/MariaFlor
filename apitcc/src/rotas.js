90d3aa5ad3ebdb00570daa9130c4d8f4d7c54567
dujosee <ra44575204891@acaonsfatima.org.br>
2024-11-22 01:06:34 -0300

    teste

--- apitcc/src/rotas.js
+++ apitcc/src/rotas.js
@@ -1,4 +1,4 @@
-import clienteController from './controller/clienteController.js'
+
 import userController from './Controller/userController.js';
 import produtoController from './controller/produtoController.js'
 import unidadeController from './Controller/unidadeController.js'
@@ -6,7 +6,6 @@ import encomendasController from './Controller/encomendasController.js'
 
 export default function adicionarRotas(servidor){
     
-   servidor.use(clienteController);
    servidor.use(userController);
    servidor.use(produtoController);
    servidor.use(unidadeController);
